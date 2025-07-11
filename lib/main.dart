f38292741c328b79884d7ab8bb672e860a095e4d
BranislavFech <xfech@stuba.sk>
2025-07-11 19:17:54 +0200

    Added initial database, I am able to connect to, write into and read from the database, although it is slow. (suggestion: optimize working with the database)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class MyDay {
+  final bool cleanedTeethMorning;
+  final bool cleanedTeethEvening;
+
+  final bool readBook;
+  final int pagesRead;
+
+  final bool coldShower;
+
+  final bool excersized;
+  final bool pushWorkout;
+  final bool pullWorkout;
+  final bool legsWorkout;
+
+  const MyDay({
+    required this.cleanedTeethEvening,
+    required this.readBook,
+    required this.pagesRead,
+    required this.coldShower,
+    required this.excersized,
+    required this.pushWorkout,
+    required this.pullWorkout,
+    required this.legsWorkout,
+    required this.cleanedTeethMorning,
+  });
+
+  Map<String, Object?> toMap() {
+    return {
+      'cleanedTeethMorning': cleanedTeethMorning ? 1 : 0,
+      'cleanedTeethEvening': cleanedTeethEvening ? 1 : 0,
+      'readBook': readBook ? 1 : 0,
+      'pagesRead': pagesRead,
+      'coldShower': coldShower ? 1 : 0,
+      'excersized': excersized ? 1 : 0,
+      'pushWorkout': pushWorkout ? 1 : 0,
+      'pullWorkout': pullWorkout ? 1 : 0,
+      'legsWorkout': legsWorkout ? 1 : 0,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'MyDay{cleanedTeethMorning: $cleanedTeethMorning, cleanedTeethEvening: $cleanedTeethEvening, readBook: $readBook, pagesRead: $pagesRead, coldShower: $coldShower, excersized: $excersized, pushWorkout: $pushWorkout, pullWorkout: $pullWorkout, legsWorkout: $legsWorkout}';
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
+
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'days_database.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE days (cleanedTeethEvening INTEGER, cleanedTeethMorning INTEGER, readBook INTEGER, pagesRead INTEGER, coldShower INTEGER, excersized INTEGER, pushWorkout INTEGER, pullWorkout INTEGER, legsWorkout INTEGER)',
+      );
+    },
+    version: 1,
+  );
+
+  Future<void> insertMyDay(MyDay myDay) async {
+    final db = await database;
+
+    await db.insert(
+      'days',
+      myDay.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  var day = MyDay(
+    cleanedTeethEvening: true,
+    readBook: true,
+    pagesRead: 7,
+    coldShower: false,
+    excersized: true,
+    pushWorkout: true,
+    pullWorkout: false,
+    legsWorkout: false,
+    cleanedTeethMorning: false,
+  );
+
+  await insertMyDay(day);
+
+  Future<List<MyDay>> days() async {
+    final db = await database;
+    final List<Map<String, Object?>> dayMaps = await db.query('days');
+
+    return dayMaps.map((map) {
+      return MyDay(
+        cleanedTeethMorning: (map['cleanedTeethMorning'] as int) == 1,
+        cleanedTeethEvening: (map['cleanedTeethEvening'] as int) == 1,
+        readBook: (map['readBook'] as int) == 1,
+        pagesRead: map['pagesRead'] as int,
+        coldShower: (map['coldShower'] as int) == 1,
+        excersized: (map['excersized'] as int) == 1,
+        pushWorkout: (map['pushWorkout'] as int) == 1,
+        pullWorkout: (map['pullWorkout'] as int) == 1,
+        legsWorkout: (map['legsWorkout'] as int) == 1,
+      );
+    }).toList();
+  }
+
+  print(await days());
+
+
 }
 
 class MyApp extends StatefulWidget {
